aab7b6b992a7c480597287034fa1d5725efff704
electrikmilk <brandonjordan124@gmail.com>
2022-10-05 14:20:21 -0400

    Fix checker argument option

--- src/main.go
+++ src/main.go
@@ -15,7 +15,7 @@ var filename string
 var css string
 
 func main() {
-	registerArg("c", "checker", "Do extra checks on the CSS file.")
+	registerArg("checker", "c", "Do extra checks on the CSS file.")
 	if len(os.Args) > 1 {
 		filename = os.Args[1]
 		if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist) {
